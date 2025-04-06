5003e48d6d2eff9107f2a4e28467358d22c5900b
KarakozN111 <156894696+KarakozN111@users.noreply.github.com>
2025-04-06 19:25:02 +0500

    lab10

---
+++ lab10/hh-front/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { CompanyListComponent } from './company-list/company-list.component';
+import { VacancyListComponent } from './vacancy-list/vacancy-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyListComponent,
+    VacancyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
